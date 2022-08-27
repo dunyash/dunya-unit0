5decb091615761c54b3bd3bbb53e35fefd17fe98
dunyash <89614617+dunyash@users.noreply.github.com>
2022-08-27 14:30:07 -0400

    Added a slider

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,12 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var tipSliderControl: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tipSliderControl.value = 15
+        
     }
     
+    @IBAction func sliderTipController(_ sender: Any) {
+        
+        let tempSliderValue = tipSliderControl.value
+        
+        rateLabel.text = String(Int(tempSliderValue)) + ("%")
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tip = (Double(Int(tempSliderValue)) * bill) / 100
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        //Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+        //rateLabel.text = String(format: "$%.0f", (sender //as AnyObject).value)
+    }
     @IBAction func calculateTip(_ sender: Any) {
         // Get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
@@ -28,10 +47,13 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
+        rateLabel.text = String(Int(tipPercentage[tipControl.selectedSegmentIndex]*100)) + ("%")
         // Update Tip Amount Label
         tipAmountLabel.text = String(format: "$%.2f", tip)
         //Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
+        //rateLabel.text = String(format: "$%.0f", (sender //as AnyObject).value)
+        
     }
     
 }
